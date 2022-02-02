e4dff9ce0fe3cebf67905a89b5ba72b3c1cef647
Dani <gohandeeo@gmail.com>
2022-02-02 09:29:19 -0600

    Ahorcado 1.0

---
+++ Ahorcado/AhorcadoLetras.py
@@ -0,0 +1,52 @@
+Palabra = "pajaro"
+list = []
+
+
+print("")
+print("     ¡========* ")
+print("    //        !") 
+print("    ||        O")
+print("    ||       -|-")
+print(F"    ||       / {chr(92)}")
+print("    ||")
+print("    ||")
+print("_-=====================-_")
+print("||                     ||")
+print("")
+print("")
+
+for i in range(len(Palabra)):
+    print("_", end= " ") 
+    list.append("_")
+print("")
+print("")
+print(list)
+contador = 10
+while 1:
+    print(f"Tienes {contador} intentos.")
+    letra = input("Escribe una letra:  ")
+    print("")
+
+    for i in range(len(Palabra)):
+        if letra == Palabra[i]:
+            list[i] = letra
+
+    print("")
+    print("")
+    print(list)
+
+    if letra in list:
+        continue
+    else:
+        contador -= 1
+        
+    
+    if contador == 0:
+        break
+
+print("")
+print("=================")
+print("G A M E   O V E R")
+print("=================")
+print("")
+
---
+++ Ahorcado/AhorcadoPalabras.py
@@ -0,0 +1,104 @@
+list = []
+
+
+print("")
+print("     ¡========* ")
+print("    //        !") 
+print("    ||        O")
+print("    ||       -|-")
+print(F"    ||       / {chr(92)}")
+print("    ||")
+print("    ||")
+print("_-=====================-_")
+print("||                     ||")
+print("+=========-=========+")
+print("Selecciona Dificultad")
+print("+=========-=========+")
+print("    1. - Facil")
+print("    2. - Media")
+print("    3. - Dificil")
+print("")
+
+
+dificultad = int(input(" -"))
+while dificultad >=4 or dificultad <=0:
+    print("Elige una opción valida.")
+    dificultad = int(input(" -"))
+    
+if dificultad == 1:
+    Palabra = "paracaidas"
+    contador = 30
+    for i in range(len(Palabra)):
+        print("_", end= " ") 
+        list.append("_")
+    list[0] = "*"
+    list[3] = "*"
+    list[4] = "*"
+
+if dificultad == 2:
+    Palabra = "helicoptero"
+    contador = 20
+    for i in range(len(Palabra)):
+        print("_", end= " ") 
+        list.append("_")
+    list[2] = "*"
+    list[5] = "*"
+
+if dificultad == 3:
+    Palabra = "supercalifrastilisticoespiralidoso"
+    contador = 10
+    for i in range(len(Palabra)):
+        print("_", end= " ") 
+        list.append("_")
+    list[0] = "*"
+
+
+print("")
+print("")
+print(list)
+
+Juego = True
+while Juego:
+    print(f"Tienes {contador} intentos.")
+    GuessWord = input("Adivina la palabra:  ")
+    while len(GuessWord) > len(Palabra):
+        print("Tu palabra excede la cantidad de caracteres.")
+        contador -= 1
+        print(f"Tienes {contador} intentos.")
+        GuessWord = input("Adivina la palabra:  ")
+    print("")
+
+    for i in range(len(GuessWord)):
+        if GuessWord[i] == Palabra[i]:
+            list[i] = GuessWord[i]
+            
+
+    print("")
+    print("")
+    print(list)
+
+    if GuessWord == Palabra:
+        print("")
+        print("")
+        print("=================")
+        print("  Y O U   W O N  ")
+        print("=================")
+        print("")
+        break
+    if GuessWord == "*":
+        for i in range(len(list)):
+            if GuessWord == list[i]:
+                list[i] = Palabra[i]
+    else:
+        contador -= 1
+        
+    if contador == 0:
+        print("")
+        print("=================")
+        print("G A M E   O V E R")
+        print("=================")
+        print("")
+        break
+
+
+
